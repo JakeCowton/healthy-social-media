d2fcfa58c252326fcad936c482e7e28544ad5f3c
Jake Cowton <jakecowton@hotmail.com>
2017-12-17 07:15:23 +0000

    Initial commit

---
+++ hide.js
@@ -0,0 +1,16 @@
+var selectedElements = document.getElementsByClassName('_ipm'), i;
+for (var i = 0; i < selectedElements.length; i ++) {
+        selectedElements[i].style.display = 'none';
+}
+var selectedElements = document.getElementsByClassName('_ipn'), i;
+for (var i = 0; i < selectedElements.length; i ++) {
+        selectedElements[i].style.display = 'none';
+}
+var selectedElements = document.getElementsByClassName('UFILikeSentence'), i;
+for (var i = 0; i < selectedElements.length; i ++) {
+        selectedElements[i].style.display = 'none';
+}
+var selectedElements = document.getElementsByClassName('UFIShareRow'), i;
+for (var i = 0; i < selectedElements.length; i ++) {
+        selectedElements[i].style.display = 'none';
+}
